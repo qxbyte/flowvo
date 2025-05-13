6d96896ab7e5e832eac5190aab5c3514ec0aca2a
xueqiang <qiang_xue0@outlook.com>
2025-05-13 11:45:23 +0800

    maven打包指定lombok版本，指定lombok编译时打包

--- api/src/main/java/org/xue/api/dto/request/milvus/InsertChunksRequest.java
+++ api/src/main/java/org/xue/api/dto/request/milvus/InsertChunksRequest.java
@@ -2,27 +2,13 @@ package org.xue.api.dto.request.milvus;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 // DTOs for request bodies
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class InsertChunksRequest {
     private String docText;
     private String docId;
-
-    public String getDocText() {
-        return docText;
-    }
-
-    public void setDocText(String docText) {
-        this.docText = docText;
-    }
-
-    public String getDocId() {
-        return docId;
-    }
-
-    public void setDocId(String docId) {
-        this.docId = docId;
-    }
 }
--- api/src/main/java/org/xue/api/dto/request/milvus/SearchChunksRequest.java
+++ api/src/main/java/org/xue/api/dto/request/milvus/SearchChunksRequest.java
@@ -2,26 +2,12 @@ package org.xue.api.dto.request.milvus;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class SearchChunksRequest {
     private String queryText;
     private int topK;
-
-    public String getQueryText() {
-        return queryText;
-    }
-
-    public void setQueryText(String queryText) {
-        this.queryText = queryText;
-    }
-
-    public int getTopK() {
-        return topK;
-    }
-
-    public void setTopK(int topK) {
-        this.topK = topK;
-    }
 }
